375e70560fde08cc643349b994117ae3dbddc871
aviraj1703 <67193881+aviraj1703@users.noreply.github.com>
2022-02-13 22:52:09 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
-const Cards = (props) => {
-    return(
-        <>
+const Cards = ({ items }) => {
+  return (
+    <>
+      <div className="Container">
+        {items.map((val) => {
+          return (
             <div id="Card">
-                <div className="Title">{props.title}</div>
-                <div className="Image">
-                    <img src={props.imgsrc} />
-                </div>
-                <div className="Link">
-                    <span>{props.pname}</span>
-                    <span><button><a href={props.link} target = "_blank">Watch&nbsp;now</a></button></span>
-                </div>
+              <div className="Title">{val.title}</div>
+              <div className="Image">
+                <img src={val.imgsrc} />
+              </div>
+              <div className="Link">
+                <span>{val.pname}</span>
+                <span>
+                  <button>
+                    <a href={val.link} target="_blank">
+                      Watch&nbsp;now
+                    </a>
+                  </button>
+                </span>
+              </div>
             </div>
-        </>
-    );
-}
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Cards;
