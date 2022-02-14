5e374b0975d1f2b7d67b4cb4dacc4dabdd98968b
aviraj1703 <67193881+aviraj1703@users.noreply.github.com>
2022-02-14 15:55:50 +0530

    Update Container.jsx

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
-import Menu from './Menubar';
-import Cards from './Card';
-import data from './Data';
+import React, { useState } from "react";
+import Menu from "./Menubar";
+import Cards from "./Card";
+import data from "./Data";
 
-const pnames = [...new Set(data.map((ce) => ce.pname)), 'All'];
+const pnames = [...new Set(data.map((ce) => ce.pname)), "All"];
 const Container = () => {
-    const [items, setItems] = useState(data);
-    const filterItem = (e) => {
-        const updateItems = data.filter((event) => {
-            return event.pname === e;
-        }) ;
-        if(e === 'All') setItems(data);
-        else    setItems(updateItems);
+  const [items, setItems] = useState(data);
+  const [wb, setwb] = useState("");
+  const filterItem = (e) => {
+    const updateItems = data.filter((event) => {
+      return event.pname === e;
+    });
+    if (e === "All") {
+      setItems(data);
+      setwb("");
     }
-    return(
-        <>
-            <Menu filterItem={filterItem} pnamess={pnames} />
-            <Cards items={items} />
-        </>
-    );
-}
+    else {
+      setItems(updateItems);
+      setwb(e);
+    }
+  };
+  return (
+    <>
+      <h1>Top {wb} Webseries in 2022</h1>
+      <Menu filterItem={filterItem} pnamess={pnames} />
+      <Cards items={items}/>
+    </>
+  );
+};
 
 export default Container;
